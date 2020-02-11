fcdc652002c376c6ddb6a6a00a682481668877be
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-02-11 13:48:27 -0700

    welcome page and results page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,37 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { MatCardModule, MatButtonModule } from '@angular/material';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    WelcomeComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
